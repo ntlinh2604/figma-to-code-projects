6abbb10bf633cb6b56ba9f8a2acb8d298117a589
Thuy Linh Nguyen-Kinnel <ntlinh2604@gmail.con>
2023-02-27 12:32:52 +0100

    edit nav and button

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 this is for all projects which created from Figma design. Mobile-first css
 1. simple-card
 2. landing-page
+3. dashboard
--- dashboard/script.js
+++ dashboard/script.js
@@ -1,13 +1,18 @@
+
+const main = document.querySelector('.main');
 const menu = document.querySelector('.toggle-menu');
 const menuIcon = document.querySelector('.menu-icon');
 
 function openMenu() {
-    return menu.style.display= "block"
+    
+    main.style.opacity= 0.2;
+    menu.style.display= "block"
 
 }
 
 function closeMenu() {
-    return menu.style.display= "none";
+    main.style.opacity= 1;
+    menu.style.display= "none";
 
 }
 
@@ -15,6 +20,7 @@ function closeMenu() {
 
 
         if((!menu.contains(e.target) && menuIcon.checkVisibility()) ){//check menu-icon display is block or not, display none == false
+            main.style.opacity= 1;
             menu.style.display = 'none'
         }
     })
